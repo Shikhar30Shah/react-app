c5485fd1b248d5fdd938e6daa2da7b8c1c89aeaf
Shah Shikhar Jayeshbhai <201951139@iiitvadodara.ac.in>
2020-08-15 17:14:44 +0530

    Component2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes : DISHES
+    };
+  }
+  render() {
   return (
     <div className="App">
       <Navbar dark color = "primary">
           <div className = "container">
-          <img src={logo} className="App-logo" alt="logo" />
-            <NavbarBrand href = "#">CLASS 2.0</NavbarBrand>
+          {/*<img src={logo} className="App-logo" alt="logo" />*/}
+            <NavbarBrand href = "/">EXXAM DESK</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes = {this.state.dishes} />
     </div>
   );
+}
 }
 
 export default App;
